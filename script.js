240dae56e98af3853dfd6910ab992d352c5456b9
Laura <lani0005@stud.kea.dk>
2024-02-05 21:23:35 +0100

    templates

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 // Step 1: Chose the <template>'s content
 const template = document.querySelector("template").content;
+console.log(template);
 
-// Step 2:
+// Step 2: Make a "clone"
+const copy = template.cloneNode(true);
+copy.querySelector("h2").textContent = "Yay!";
+
+const parent = document.querySelector(".grid");
+parent.appendChild(copy);
+
+// Step 3: Change the content
+
+// Step 4: Choose the new "parent" element
+
+// Step 5: Add (Append) the clone to the DOM
