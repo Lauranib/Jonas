8fd3b403f2f6bd4959e4be58415122e0b8e21c62
Laura <lani0005@stud.kea.dk>
2024-02-07 19:41:23 +0100

    added html

--- script.js
+++
@@ -1,7 +0,0 @@
-const template = document.querySelector("template").content;
-console.log(template);
-
-const copy = template.cloneNode(true);
-
-const parent = document.querySelector(".grid");
-parent.appendChild(copy);
