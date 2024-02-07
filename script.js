496972e659bc5ee3be0b29733774176368e99b55
Laura <lani0005@stud.kea.dk>
2024-02-07 19:15:36 +0100

    ew

--- script.js
+++ script.js
@@ -1,31 +1,7 @@
-const data = {
-  header: "Bussen er kørt!",
-  description: "Vi når den næste",
-};
-
 const template = document.querySelector("template").content;
 console.log(template);
 
 const copy = template.cloneNode(true);
-copy.querySelector("h2").textContent = data.header;
-copy.querySelector("p").textContent = data.description;
 
 const parent = document.querySelector(".grid");
 parent.appendChild(copy);
-
-const ninjas = [
-  {
-    navn: "Cole",
-    power: "earth",
-  },
-  {
-    navn: "Zane",
-    power: "ice",
-  },
-];
-
-ninjas.forEach(doStuff);
-
-function doStuff(aNinja) {
-  console.log(aNinja.name + " has the power " + aNinja.power);
-}
